705d6a9d2915035725eed4bb4c1c6fb23701c917
gusta <tavinhocassis@github>
2024-03-07 09:07:45 -0300

    trabalho de ds

--- gusta/MainPage.xaml.cs
+++ gusta/MainPage.xaml.cs
@@ -15,7 +15,21 @@ public partial class MainPage : ContentPage
       Application.Current.MainPage = new GamePage();
   }
   
-
+   private void Botaosobre(object sender, EventArgs args)
+  {
+    sobre.IsVisible = true;
+  }
   
+  private void voltarmenu (object sender, EventArgs args)
+  {
+    if (Application.Current != null)
+      Application.Current.MainPage = new MainPage();
+  }
+
+
+
+
+
+
 }
 

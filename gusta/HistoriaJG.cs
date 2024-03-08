310f9e1ce582ede81ab5cccebf3fbd6c85cfdcbb
gusta <brazuca17@github>
2024-03-08 11:09:39 -0300

    trabalhode ds

--- gusta/HistoriaJG.cs
+++ gusta/HistoriaJG.cs
@@ -1,23 +1,17 @@
 public class Historia
 {
    public int Id;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   public string? Texto;
+   public bool TemImagem = false;
+   public string? UrlDaImagem;
+   public bool TemResposta = false;
+   public string? TextoDaResposta01;
+   public string? TextoDaResposta02;
+   public string? TextoDaResposta03;
+   public int IdLevelResposta01;
+   public int IdLevelResposta02;
+    public int IdLevelResposta03;
+     public bool HeroIsDead = false;
 
 
 }
--- gusta/MainPage.xaml.cs
+++ gusta/MainPage.xaml.cs
@@ -29,7 +29,5 @@ public partial class MainPage : ContentPage
 
 
 
-
-
 }
 

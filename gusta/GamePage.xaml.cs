06d35f62cee78b52746e068ec1fac8e792c339de
Brazuca17 <127317817+Brazuca17@users.noreply.github.com>
2024-03-27 10:39:17 -0300

    Update GamePage.xaml.cs

--- gusta/GamePage.xaml.cs
+++ gusta/GamePage.xaml.cs
@@ -15,112 +15,19 @@ public GamePage()
       Texto = "ola viajante",
       TemResposta=false
     }); 
-    historia.Add(new HistoriaJG()
-    {
-      Id = 1,
-      Texto = "tedtsytdydyusadt",
-      TemResposta=true,
-      TextoDaResposta01="Sim, por favor",
-      TextoDaResposta02="Não, estou bem",
-      TextoDaResposta03="Para de me encher",
-      IdLevelResposta01=2,
-      IdLevelResposta02=4998,
-      IdLevelResposta03=5000
-    });
-    historia.Add(new HistoriaJG()
-    {
-      Id = 2,
-      Texto = "SFLKDVHSFDVCVFHJVASA",
-      TemResposta=false,
-    });
-    historia.Add(new HistoriaJG()
-    {
-      Id = 3,
-      Texto = "FGYUDFGHFDHF",
-      TemResposta=false,
-    });
-     historia.Add(new HistoriaJG()
-    {
-      Id = 4,
-      Texto = "DSDSDASDSADSADSAD?",
-      TemResposta=false,
-    });
-    historia.Add(new HistoriaJG()
-    {
-      Id = 5,
-      Texto = "KSHDJHFSJKDFDFSDFDFSD",
-      HeroIsDead=true
-    });
-    historia.Add(new HistoriaJG()
-    {
-      Id = 4998,
-      Texto = "SFJDFSDFHDJHSGDsa",
-      TemResposta=false,
-    });
-    historia.Add(new HistoriaJG()
-    {
-      Id = 4999,
-      Texto = "SADSADSADSADSAD",
-      TemResposta=false,
-      HeroIsDead=true
-    });
-    historia.Add(new HistoriaJG()
-    {
-      Id = 5000,
-      Texto = "DSHDSAGDSAHD",
-      TemResposta=false,
-      HeroIsDead=true
-    });
-
-    Iniciar();
-
-	}
-
+    
    void Iniciar()
   {
     TrocaHistoriaJGatual(0);
   }
 
-  void PreencherPagina()
-  {
-       textodalabel.Text = HistoriaJGatual.Texto;
-
-    if (HistoriaJGatual.HeroIsDead)
-       morte.IsVisible = true;
-    else
-       morte.IsVisible = false;
-
-    if (HistoriaJGatual.TemResposta)
-    {
-      next.IsVisible = false;
-      buttonResposta01.IsVisible = true;
-      buttonResposta02.IsVisible = true;
-      buttonResposta03.IsVisible = true;
-      buttonResposta01.Text = HistoryStepAtual.TextoDaResposta01;
-      buttonResposta02.Text = HistoryStepAtual.TextoDaResposta02;
-      buttonResposta03.Text = HistoryStepAtual.TextoDaResposta03;
-    }
-    else
-    {
-      next.IsVisible = true;
-      buttonResposta01.IsVisible = false;
-      buttonResposta02.IsVisible = false;
-      buttonResposta03.IsVisible = false;
-    }
-  }
-
+ 
   void onnext(object sender, EventArgs args)
   {
     var proximoId = HistoriaJG.Id + 1;
     TrocaHistoriaJGatual(proximoId);
   }
-
-  void TrocaHistoriaJGatual(int id)
-  {
-    HistoriaJG = historia.Where(d => d.Id == id).First();
-    PreencherPagina();
-  }
-
+  
   void OnButton01Clicked(object sender, EventArgs args)
   {
     TrocaHistoriaJGatual(HistoriaJGatual.IdLevelResposta01);
